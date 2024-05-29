85bbbe0987042fa2135cde0b17248e90ca8ac2dd
illya-dev <illiashevdev@gmail.com>
2024-05-29 16:09:49 +0200

    Update eslint version. Add google fonts. Add Inter & Poppins fonts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
   ],
   css: ['~/assets/css/main.css'],
   devServer: {
     port: 7777,
   },
+  googleFonts: {
+    families:{
+      Poppins: [300, 400, 500],
+      Inter: [300, 400, 500, 600]
+    }
+  }
 })
